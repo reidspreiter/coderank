2b70f3b7e2948d9e408e0f33ddbd116484fcaaff
reidspreiter <reidspreiter@gmail.com>
2024-10-05 17:42:09 -0500

    src/schemata: add Zod schemas and corresponding types
    Zod allows schemas to be more flexible and easier to change in
    the future if any additional fields are added.
    
    Zod schemas now categorize typing data by programming language.
    
    Moreover, the cumbersome addFields, convertFields, and buildFields
    helper functions have been removed. Zod schemas can easily emulate
    buildFields, and are much more enjoyable to use. Additional helper
    functions are available to add certain schemata types to others, and
    are once again, easier to expand if needed.

---
+++ src/schemata/schemata.ts
@@ -0,0 +1,64 @@
+import { z } from "zod";
+
+export const CharMapSchema = z.record(z.number()).default({});
+export type CharMap = z.infer<typeof CharMapSchema>;
+
+export const LanguageSchema = z.object({
+    language: z.string().default("unknown"),
+    added: z.number().default(0),
+    deleted: z.number().default(0),
+});
+export type Language = z.infer<typeof LanguageSchema>;
+
+export const LanguageWithCharsSchema = LanguageSchema.extend({
+    chars: CharMapSchema,
+});
+export type LanguageWithChars = z.infer<typeof LanguageWithCharsSchema>;
+
+export const FieldsSchema = z.object({
+    rank: z.number().default(0),
+    net: z.number().default(0),
+    added: z.number().default(0),
+    deleted: z.number().default(0),
+    chars: CharMapSchema,
+    rankBuffer: z.number().default(0),
+});
+export type Fields = z.infer<typeof FieldsSchema>;
+
+export const FieldsWithLanguageSchema = FieldsSchema.extend({
+    languages: z.array(LanguageSchema).default([]),
+});
+export type FieldsWithLanguage = z.infer<typeof FieldsWithLanguageSchema>;
+
+export const WeeklyFieldsSchema = FieldsSchema.extend({
+    week: z.number().default(0),
+    languages: z.array(LanguageWithCharsSchema).default([]),
+});
+export type WeeklyFields = z.infer<typeof WeeklyFieldsSchema>;
+
+export const TotalFieldsSchema = z.object({
+    rank: z.number().default(0),
+    net: z
+        .string()
+        .default("0")
+        .transform((x) => BigInt(x)),
+    added: z
+        .string()
+        .default("0")
+        .transform((x) => BigInt(x)),
+    deleted: z
+        .string()
+        .default("0")
+        .transform((x) => BigInt(x)),
+    chars: CharMapSchema,
+    rankBuffer: z.number().default(0),
+    languages: z.array(LanguageSchema).default([]),
+});
+export type TotalFields = z.infer<typeof TotalFieldsSchema>;
+
+export const StatsSchema = z.object({
+    year: z.number(),
+    total: FieldsWithLanguageSchema,
+    weeks: z.array(WeeklyFieldsSchema),
+});
+export type Stats = z.infer<typeof StatsSchema>;
