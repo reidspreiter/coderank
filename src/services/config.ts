487b7507c63c456b43d9b37dcaddc7993d080f7b
reidspreiter <reidspreiter@gmail.com>
2025-02-10 22:20:35 -0600

    src/services/config: remove deprecated settings
    The refreshRate setting is no longer necessary as Coderank v0.4.0 will
    not render buffer contents as it overcomplicates things and is useless.
    
    The trackChars setting is deprecated because chars are the source of
    a significant amount of data trends. If users truly wish for this
    feature, it can easily be reincorporated in the future.

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,22 +1,16 @@
 import { workspace } from "vscode";
 
 export type Config = {
-    refreshRate: number;
     saveCredentials: boolean;
-    trackChars: boolean;
     autoStore: boolean;
-    loadLocalOnStart: boolean;
     debug: boolean;
 };
 
 export function getConfig(): Config {
     const config = workspace.getConfiguration("coderank");
     return {
-        refreshRate: config.get<number>("refreshRate", 100),
         saveCredentials: config.get<boolean>("saveCredentials", false),
-        trackChars: config.get<boolean>("trackCharacters", true),
         autoStore: config.get<boolean>("autoStore", true),
-        loadLocalOnStart: config.get<boolean>("loadLocalOnStart", true),
         debug: config.get<boolean>("debug", false),
     };
 }
